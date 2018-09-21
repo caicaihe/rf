da73c138c9012e2682dda09ffb77bfbfa7d2d014
caicaihe <heliping@caicloud.io>
2018-09-21 15:37:04 +0800

    update

---
+++ UI/.metadata/.plugins/org.eclipse.core.resources/.history/65/e034039d70bd001815e4c20ec6dc4efe
@@ -0,0 +1,8 @@
+from .myseleniumKeywords import myseleniumKeywords
+
+
+class myselenium(myseleniumKeywords):
+    """ this is init info
+    """
+
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
---
+++ compass/devops/RobotCustomizdeLibraries/myselenium/myseleniumKeywords.py
@@ -0,0 +1,10 @@
+class myseleniumKeywords(object):
+    """
+    my class info
+    """
+    ROBOT_LIBRARY_SCOPE = 'Global'
+    def myopen(self):
+        """
+        my funciont info
+        """
+        print("hello")
